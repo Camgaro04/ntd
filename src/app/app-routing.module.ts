4dfa2908f48d68e97e65dc0d093a5fa430e9ef50
Andres Hernandez <phernandez@asesoftware.com>
2019-02-14 11:19:48 -0500

    Coomit Andres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './Pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  {path: 'home', component: InicioComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Pages/footer/footer.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
