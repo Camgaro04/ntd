ef50a45e1e99388f7ceedd88436a28a7364c1daa
Andres Hernandez <phernandez@asesoftware.com>
2019-02-14 11:50:00 -0500

    Se ajusta error en Styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './Pages/inicio/inicio.component';
+import { CourseInfoComponent } from './component/course-info/course-info.component';
 
 const app_routes: Routes = [
   {path: 'home', component: InicioComponent},
-  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'info', component: CourseInfoComponent}
 
 ];
 
