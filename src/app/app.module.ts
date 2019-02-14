14fd1a519274ebd1f3a82c02ce1a38385dfb6b14
camgaro <jcgarcia722@misena.edu.co>
2019-02-14 02:03:52 -0500

    Se agrega modulo informacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './component/course-info/course-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
